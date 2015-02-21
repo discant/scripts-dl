5571a7a834f5a3669727dcf53e090b292fff913e
Xander Smeets <xsmeets@discant.net>
2015-02-21 12:46:50 +0100

    Update technic.bat
    Updated URL

--- technic.bat
+++ technic.bat
@@ -6,6 +6,6 @@ title Command Download Batch
 
 md c:\net
 
-call bitsadmin /transfer NETFX  /download /priority normal http://launcher.technicpack.net/launcher/431/TechnicLauncher.exe c:\users\%USERNAME%\desktop\TechnicLauncher.exe
+call bitsadmin /transfer NETFX  /download /priority normal http://launcher.technicpack.net/launcher4/272/TechnicLauncher.exe c:\users\%USERNAME%\desktop\TechnicLauncher.exe
 
 pause
